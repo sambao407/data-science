6a14bae82e3683ac694ce0a67c73d7400252f65d
Samba <sambao407@gmail.com>
2020-12-14 15:13:47 +0100

    Add new matrix

--- sharpening.py
+++ sharpening.py
@@ -2,17 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
 
-img = cv.imread('datasets/Blurry/blurry_003.jpg')
+img = cv.imread('datasets/Blurry/blurry_001.jpg')
 
 #filter2D
-kernel = np.ones((3,3),np.float32)/9
+#kernel = np.ones((3,3),np.float32)/9
+kernel = np.array([[0, -1, 0],
+                   [-1, 5,-1],
+                   [0, -1, 0]])
 dst= dst = cv.filter2D(img,-1,kernel)
-
-plt.subplot(121),plt.imshow(img),plt.title('Original')
-plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
-plt.xticks([]), plt.yticks([])
-plt.show()
+window_name = 'image'
+cv.imshow(window_name,dst)
+cv.waitKey(0)
 
 
 #blur
